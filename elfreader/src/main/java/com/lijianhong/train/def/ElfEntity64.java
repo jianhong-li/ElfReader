e2eb5aa236981aaf5d26548f69d1d784b94ca038
jianhongli <qunar.jianhong.li@gmail.com>
2022-07-18 00:39:57 +0800

    MOD: 开始解析 段表.

--- elfreader/src/main/java/com/lijianhong/train/def/ElfEntity64.java
+++ elfreader/src/main/java/com/lijianhong/train/def/ElfEntity64.java
@@ -1,13 +1,18 @@
 package com.lijianhong.train.def;
 
+import com.lijianhong.train.reader.ReadUtils;
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author lijianhong Date: 2022/7/17 Time: 4:24 PM
  * @version $Id$
  */
 public class ElfEntity64 {
+
+    private static Logger logger = LoggerFactory.getLogger(ElfEntity64.class);
 
     public ElfHeader64 elfHeader = new ElfHeader64();
 
@@ -16,7 +21,20 @@ public class ElfEntity64 {
 
 
     public void initShdr(byte[] fileBytes) {
-        long baseOffset = elfHeader.e_shoff;
+        int baseOffset =(int) elfHeader.e_shoff;
+        int sectionTableNumber = elfHeader.e_shnum;
 
+        logger.info("段表基础偏移为:{} ,段个数为:{}", baseOffset, sectionTableNumber);
+        for (int i = 0; i < sectionTableNumber; i++) {
+
+            baseOffset += i * Shdr64.size;
+
+            Shdr64 shdr64 = new Shdr64();
+            shdr64.init(fileBytes, baseOffset);
+            shdr64._index = i;
+            shdr64List.add(shdr64);
+
+            shdr64.printInfo();
+        }
     }
 }
--- elfreader/src/main/java/com/lijianhong/train/def/Shdr64.java
+++ elfreader/src/main/java/com/lijianhong/train/def/Shdr64.java
@@ -1,10 +1,18 @@
 package com.lijianhong.train.def;
 
+import com.lijianhong.train.reader.ReadUtils;
+import com.lijianhong.train.util.Hex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author lijianhong Date: 2022/7/17 Time: 10:32 PM
  * @version $Id$
  */
 public class Shdr64 {
+
+    private static Logger logger = LoggerFactory.getLogger(Shdr64.class);
+
     // typedef struct elf64_shdr {
     //  Elf64_Word sh_name;		/* Section name, index in string tbl */
     //  Elf64_Word sh_type;		/* Type of section */
@@ -18,6 +26,10 @@ public class Shdr64 {
     //  Elf64_Xword sh_entsize;	/* Entry size if section holds table */
     //} Elf64_Shdr;
 
+    public static final int size = 4 + 4 + 8 + 8 + 8 + 8 + 4 + 4 + 8 + 8;
+
+    public int _index;
+
     public int sh_name;        /* Section name, index in string tbl */
     public int sh_type;        /* Type of section */
     public long sh_flags;        /* Miscellaneous section attributes */
@@ -28,4 +40,23 @@ public class Shdr64 {
     public int sh_info;        /* Additional section information */
     public long sh_addralign;    /* Section alignment */
     public long sh_entsize;    /* Entry size if section holds table */
+
+    public void init(byte[] fileBytes, int baseOffset) {
+            this.sh_name    = ReadUtils.readWord(fileBytes,        baseOffset);
+            this.sh_type    = ReadUtils.readWord(fileBytes, baseOffset + 4);
+            this.sh_flags   = ReadUtils.readLong(fileBytes, baseOffset + 4 + 4);
+            this.sh_addr    = ReadUtils.readLong(fileBytes, baseOffset + 4 + 4 + 8);
+            this.sh_offset  = ReadUtils.readLong(fileBytes, baseOffset + 4 + 4 + 8 + 8);
+            this.sh_size    = ReadUtils.readLong(fileBytes, baseOffset + 4 + 4 + 8 + 8 + 8);
+            this.sh_link    = ReadUtils.readWord(fileBytes, baseOffset + 4 + 4 + 8 + 8 + 8 + 8);
+            this.sh_info    = ReadUtils.readWord(fileBytes, baseOffset + 4 + 4 + 8 + 8 + 8 + 8 + 4);
+        this.sh_addralign   = ReadUtils.readLong(fileBytes, baseOffset + 4 + 4 + 8 + 8 + 8 + 8 + 4 + 4);
+        this.sh_entsize     = ReadUtils.readLong(fileBytes, baseOffset + 4 + 4 + 8 + 8 + 8 + 8 + 4 + 4 + 8);
+    }
+
+    public void printInfo() {
+
+        logger.info("sh[{}]sh_name:{}", _index,Hex.toHex(sh_name));
+    }
+
 }
